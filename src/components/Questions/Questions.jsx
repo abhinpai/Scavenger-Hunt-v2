4d3383eadeaab6f5448e7befb8122a27446cada6
Abhin Pai <abhinpai96@gmail.com>
2020-09-25 16:23:14 +0530

    Added the toch view

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -2,12 +2,30 @@ import React, { useEffect } from 'react';
 import './Questions.scss';
 import QuestionsImg from '../../resources/images/question_bg.png';
 import ClockImg from '../../resources/images/clock.png';
-import { SimpleImg } from 'react-simple-img';
 
 function Questions() {
   useEffect(() => {
     document.getElementById('app-body').classList.add('dark-body-bg');
+    console.log(document.getElementById('q-Img'));
+    document.getElementById('q-Img').addEventListener('mousemove', update);
+    document.getElementById('q-Img').addEventListener('touchmove', update);
   }, []);
+
+  function update(e) {
+    var x = e.clientX || e.touches[0].clientX;
+    var y = e.clientY || e.touches[0].clientY;
+    document.getElementById('q-Img').style.setProperty('--cursorX', x + 'px');
+    document.getElementById('q-Img').style.setProperty('--cursorY', y + 'px');
+  }
+
+  const trunOnTorch =() =>{
+    document.getElementById('q-Img').classList.add('spotlight');
+  }
+
+  const trunOffTorch =() =>{
+    document.getElementById('q-Img').classList.remove('spotlight');
+  }
+
   return (
     <div className='question'>
       <header className='question__header'>
@@ -26,19 +44,17 @@ function Questions() {
       </header>
       <main className='questions-div'>
         <div className='questions-div__block'>
-          <SimpleImg
-            width={680}
-            height={450}
-            importance={'high'}
-            src={QuestionsImg}
-            alt='banner'
-          />
+          <div
+            id='q-Img'
+            className='question-img'
+            style={{ backgroundImage: `url(${QuestionsImg})` }}
+          ></div>
           <input placeholder='Enter your answer...' />
           <div className='questions-div__block__btns'>
-            <div className='questions-div__block__btns__btn  wodden-btn'>
+            <div onClick={trunOnTorch} className='questions-div__block__btns__btn  wodden-btn'>
               <p>Clue</p>
             </div>
-            <div className='questions-div__block__btns__btn  wodden-btn'>
+            <div onClick={trunOffTorch} className='questions-div__block__btns__btn  wodden-btn'>
               <p>Skip</p>
             </div>
             <div
